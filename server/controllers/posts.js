b4e96d49955433ab6f4c56847ba4cdc7bd327672
Rui Zhang <rza21.sfu@gmail.com>
2022-04-02 12:11:15 -0700

    add models

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,16 @@
-export const getPosts = (req, res) => {
-    res.send('Works!');
+import PostMessage from "../models/postMessage.js";
+export const getPosts = async (req, res) => {
+    const postMessages = await PostMessage.find();
+    try {
+        
+        console.log(postMessage);
+        res.status(200).json(postMessages);
+    } catch(error) {
+        res.status(404).json({ message : error.message });
+    }
+}
+
+export const createPost = (req, res) => {
+    res.send('Post Creation');
 }
 
